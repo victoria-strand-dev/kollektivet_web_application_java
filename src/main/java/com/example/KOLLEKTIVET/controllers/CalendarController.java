537bb0ceef53e2150be0f3dd6af5686501cc3d0b
Victoria Strand <vice.s.s@hotmail.com>
2026-06-04 18:20:49 +0200

    Import CrossOrigin in CalendarController

--- src/main/java/com/example/KOLLEKTIVET/controllers/CalendarController.java
+++ src/main/java/com/example/KOLLEKTIVET/controllers/CalendarController.java
@@ -4,13 +4,9 @@ import com.example.KOLLEKTIVET.models.CalendarResponse;
 import com.example.KOLLEKTIVET.models.CalendarEvent;
 import com.example.KOLLEKTIVET.repositories.CalendarEventRepository;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
